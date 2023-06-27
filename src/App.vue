ad74d22a2878e51214aab5f42b11b5f7c05a5090
Zhomart Zhetpisbekov <zhetpisbekovzh@gmail.com>
2023-06-27 15:37:12 +0600

    feat: added add-task functionality

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,35 @@ export default {
   },
 
   methods: {
+    async addTask(newTask) {
+      const res = await fetch('http://localhost:5000/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(newTask)
+      })
+
+      const data = await res.json()
+
+      this.tasks = [...this.tasks, data]
+
+      this.showAddTask = false
+    },
+
+    toggleAddTask() {
+      if (this.showAddTask) {
+        this.changeTask = false
+      }
+
+      this.showAddTask = !this.showAddTask
+      if (this.showAddTask) {
+        this.buttonTitle = 'Close'
+      } else {
+        this.buttonTitle = 'Add Task'
+      }
+    },
+
     async deleteTask(id) {
       const res = await fetch(`http://localhost:5000/tasks/${id}`, {
         method: 'DELETE'
@@ -49,6 +78,60 @@ export default {
         alert('Error with deleting the task')
       }
     },
+    async openTaskEditor(id) {
+      this.taskToEdit = await this.fetchTask(id)
+      this.changeTask = true
+      this.showAddTask = true
+
+      // const taskToToggle = await this.fetchTask(id)
+      // const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+      // const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      //   method: "PUT",
+      //   headers: {
+      //     "Content-type": "application/json"
+      //   },
+      //   body: JSON.stringify(updTask)
+      // })
+
+      // const data = await res.json()
+
+      // this.tasks = this.tasks.map(
+      //   (task) => task.id === id ?
+      //   {...task, reminder: data.reminder} :
+      //   task
+      // )
+    },
+    async editTask(newTask) {
+      // Deleting the task with ID
+      const res = await fetch(`http://localhost:5000/tasks/${newTask.id}`, {
+        method: 'DELETE'
+      })
+
+      if (res.status === 200) {
+        this.tasks = this.tasks.filter((task) => task.id != newTask.id)
+      } else {
+        alert('Error with deleting the task')
+      }
+
+      // Adding task with same ID
+      const resp = await fetch('http://localhost:5000/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(newTask)
+      })
+
+      const data = await resp.json()
+
+      this.tasks = [...this.tasks, data]
+
+      //
+      this.changeTask = false
+      this.showAddTask = false
+    },
+
     async fetchTasks() {
       const res = await fetch('http://localhost:5000/tasks')
 
